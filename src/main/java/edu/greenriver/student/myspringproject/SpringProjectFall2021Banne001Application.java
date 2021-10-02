6b4c9bdf3cddfc5b2daf18e5820257236c7206cd
banne001 <blezylsantos@yahoo.com>
2021-10-02 11:35:08 -0700

    Issue #1 closed: Finished creating a new project

---
+++ src/main/java/edu/greenriver/student/myspringproject/SpringProjectFall2021Banne001Application.java
@@ -0,0 +1,13 @@
+package edu.greenriver.student.myspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectFall2021Banne001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectFall2021Banne001Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/greenriver/student/myspringproject/SpringProjectFall2021Banne001ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.greenriver.student.myspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectFall2021Banne001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
