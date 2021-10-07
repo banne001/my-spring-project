9ad89627b391e206423ae1319715020595c79f77
banne001 <blezylsantos@yahoo.com>
2021-10-06 23:07:45 -0700

    Update: Added comments throughout project

--- src/main/java/edu/greenriver/student/myspringproject/controllers/IndexController.java
+++ src/main/java/edu/greenriver/student/myspringproject/controllers/IndexController.java
@@ -3,8 +3,20 @@ package edu.greenriver.student.myspringproject.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Index Controller to route users to correct web pages
+ *
+ * @author blezyl santos
+ * @version 1.0
+ */
 @Controller
 public class IndexController {
+
+    /**
+     * Map for main pages
+     * include "", "/", "index", "index.html"
+     * @return html page
+     */
     @RequestMapping(value = {"", "/", "index", "index.html"})
     public String home() {
         return "home";
